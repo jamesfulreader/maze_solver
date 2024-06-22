184353b3cf724003e5efffc3d4907c5d968c1bc4
jamesfulreader <jamesfulreader@gmail.com>
2024-06-21 20:26:59 -0600

    create tests update all files to match course files to resolve errors

--- cell.py
+++ cell.py
@@ -1,50 +1,62 @@
 from graphics import Line, Point
 
+
 class Cell:
-    def __init__(self, window) -> None:
-        self.has_left_w = True
-        self.has_right_w = True
-        self.has_top_w = True
-        self.has_bottom_w = True
-        self.__x1 = None
-        self.__y1 = None
-        self.__x2 = None
-        self.__y2 = None
-        self.__win = window
-    
+    def __init__(self, win=None):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+
     def draw(self, x1, y1, x2, y2):
-        self.__x1 = x1
-        self.__y1 = y1
-        self.__x2 = x2
-        self.__y2 = y2
-        if self.has_left_w:
-            line = Line(Point(x1,y1), Point(x1,y2))
-            self.__win.draw_line(line)
-        
-        if self.has_right_w:
-            line = Line(Point(x1,y1), Point(x2,y1))
-            self.__win.draw_line(line)
+        if self._win is None:
+            return
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, "white")
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "white")
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "white")
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, "white")
 
-        if self.has_top_w:
-            line = Line(Point(x2,y1), Point(x2,y2))
-            self.__win.draw_line(line)
-        
-        if self.has_bottom_w:
-            line = Line(Point(x1,y2), Point(x2,y2))
-            self.__win.draw_line(line)
-    
     def draw_move(self, to_cell, undo=False):
-        half_length = abs(self.__x2 - self.__x1) // 2
-        x_center = half_length + self.__x1
-        y_center = half_length + self.__y1
+        half_length = abs(self._x2 - self._x1) // 2
+        x_center = half_length + self._x1
+        y_center = half_length + self._y1
 
-        half_length2 = abs(to_cell.__x2 - to_cell.__x1) // 2
-        x_center2 = half_length2 + to_cell.__x1
-        y_center2 = half_length2 + to_cell.__y1
+        half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
+        x_center2 = half_length2 + to_cell._x1
+        y_center2 = half_length2 + to_cell._y1
 
         fill_color = "red"
         if undo:
             fill_color = "gray"
 
         line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
-        self.__win.draw_line(line, fill_color)
+        self._win.draw_line(line, fill_color)
--- maze.py
+++ maze.py
@@ -1,41 +1,59 @@
-from time import sleep
 from cell import Cell
+import random
+import time
+
 
 class Maze:
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
-        self.__x1 = x1
-        self.__y1 = y1
-        self.__num_rows = num_rows
-        self.__num_cols = num_cols
-        self.__cell_size_x = cell_size_x
-        self.__cell_size_y = cell_size_y
-        self.__win = win
-        self.__cells = []
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win=None,
+    ):
+        self._cells = []
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
 
-        self.__create_cells()
-    
-    def __create_cells(self):
-        for i in range(self.__num_cols):
-            col = []
-            for j in range(self.__num_rows):
-                col.append(Cell(self.__win))
-            self.__cells.append(col)
+        self._create_cells()
+        self._break_entrance_and_exit()
 
-        for i in range(self.__num_cols):
-            for j in range(self.__num_rows):
-                self.__draw_cell(i, j)
+    def _create_cells(self):
+        for i in range(self._num_cols):
+            col_cells = []
+            for j in range(self._num_rows):
+                col_cells.append(Cell(self._win))
+            self._cells.append(col_cells)
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i, j)
 
-    def __draw_cell(self, i, j):
-        if self.__win is None:
+    def _draw_cell(self, i, j):
+        if self._win is None:
             return
-        x1 = self.__x1 + i * self.__cell_size_x
-        y1 = self.__y1 + j * self.__cell_size_y
-        x2 = x1 + self.__cell_size_x
-        y2 = y1 + self.__cell_size_y
+        x1 = self._x1 + i * self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
 
-        self.__cells[i][j].draw(x1, y1, x2, y2)
-        self.__animate()
-    
-    def __animate(self):
-        self.__win.redraw()
-        sleep(0.05)
+    def _animate(self):
+        if self._win is None:
+            return
+        self._win.redraw()
+        time.sleep(0.05)
+
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0, 0)
+        self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
+        self._draw_cell(self._num_cols - 1, self._num_rows - 1)
